d33a67d5b9896da82772f6bddfe1931f3795df33
Kun Zhao <zhaokun.km@gmail.com>
2022-01-05 23:03:12 +0800

    Add a dedicated web server for output statistics of the stressing test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::{App, Arg};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use clap::Arg;
 use config::Config;
 use mqtt::{packet::*, Encodable};
 use rand::{self, Rng};
@@ -19,10 +20,11 @@ enum StressState {
     Published,
     Publishing,
 }
-
-#[tokio::main]
-async fn main() {
-    let matches = App::new("MQTT stress test program")
+async fn hello_world() -> impl Responder {
+    "Hello World!"
+}
+fn main() {
+    let matches = clap::App::new("MQTT stress test program")
         .version("0.1.0")
         .author("Kun Zhao")
         .about("MQTT stress test program")
@@ -36,7 +38,18 @@ async fn main() {
         .get_matches();
 
     let path = matches.value_of("file").unwrap_or("config.yml");
+    actix_web::rt::System::with_tokio_rt(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .worker_threads(8)
+            .thread_name("main-tokio")
+            .build()
+            .unwrap()
+    })
+    .block_on(async_main(path));
+}
 
+async fn async_main(path: &str) {
     let config = config::Config::from_file(path).unwrap();
     let connection = config.connection;
     let mut handles = vec![];
@@ -46,8 +59,23 @@ async fn main() {
         let client = cfg.client_id.clone() + &i.to_string();
         handles.push(tokio::spawn(stressing(client, cfg)))
     }
-    futures::future::join_all(handles).await;
-    println!("All tasks run finished");
+    tokio::spawn(async {
+        futures::future::join_all(handles).await;
+        println!("All tasks run finished");
+    });
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(actix_web::middleware::Logger::default())
+            .route("/", web::get().to(hello_world))
+        //.service(index)
+    })
+    .workers(2)
+    .bind("0.0.0.0:8088")
+    .expect("Couldn't bind to port 8088")
+    .run()
+    .await
+    .unwrap();
 }
 
 async fn stressing(client: String, cfg: Arc<Config>) {
